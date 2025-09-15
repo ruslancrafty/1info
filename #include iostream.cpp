c5bfe8448666a41737554bca549f0eb30698345f
ruslancrafty <ruslan_crafty@mail.ru>
2025-09-16 00:06:45 +0300

    Add files via upload

---
+++ #include iostream.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // объявление и инициализация массива размером 5
+    int arr[5] = {2, 4, 8, 12, 16};
+
+    // печать элементов массива
+    for (int i = 0; i < 5; i++) {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+    }
+
+#include <iostream>
+#include <stack>
+ 
+int main()
+{
+    std::stack<std::string> stack;    // пустой стек строк
+}
---
+++ python.py
@@ -0,0 +1,11 @@
+MAS = ['QQ', 'WWW', 'EEE', 'RRR', 'TTT', 'YYY']
+MAS.append('DDD')
+
+
+class Stack:
+    def __init__():
+        pass
+    def pop():
+        pass
+    def push():
+        pass
