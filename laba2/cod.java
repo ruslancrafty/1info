caa86b6808df198421b5a5fa2ad701002e981698
ruslancrafty <ruslan_crafty@mail.ru>
2025-11-17 11:25:10 +0300

    Create cod.java

---
+++ laba2/cod.java
@@ -0,0 +1,25 @@
+Мультисписок (двусвязный список)
+class Node {
+    int data;
+    Node prev, next;
+    Node(int data) { this.data = data; }
+}
+'---------------------'
+Очередь
+import java.util.*;
+Queue<Integer> q = new LinkedList<>();
+q.add(1);
+q.poll(); 
+'------------------------'
+Дек
+Deque<Integer> dq = new ArrayDeque<>();
+dq.addLast(1);
+dq.addFirst(0);
+dq.pollLast();
+dq.pollFirst();
+'----------------------'
+Приоритетная очередь
+PriorityQueue<Integer> pq = new PriorityQueue<>();
+pq.offer(10);
+pq.offer(5);
+pq.poll(); // 5 (min-heap по умолчанию)
